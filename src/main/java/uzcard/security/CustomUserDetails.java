7640b41ab5352a1b5fa86999a80c2f95425f5276
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-12-13 11:22:00 +0500

    Spring Security added

---
+++ src/main/java/uzcard/security/CustomUserDetails.java
@@ -0,0 +1,80 @@
+package uzcard.security;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import uzcard.profile.ProfileEntity;
+import uzcard.profile.ProfileRole;
+import uzcard.profile.ProfileStatus;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+    private String id;
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private ProfileStatus status;
+    @Enumerated(EnumType.STRING)
+    private ProfileRole role;
+    private LocalDateTime createdDate;
+
+    public CustomUserDetails(ProfileEntity entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.surname = entity.getSurname();
+        this.username = entity.getUsername();
+        this.password = entity.getPassword();
+        this.status = entity.getStatus();
+        this.role = entity.getRole();
+        this.createdDate = entity.getCreatedDate();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(role.name()));
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return status.equals(ProfileStatus.ACTIVE);
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true ;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
---
+++ src/main/java/uzcard/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package uzcard.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import uzcard.exceptionhandler.ResourceNotFoundException;
+import uzcard.profile.ProfileEntity;
+import uzcard.profile.ProfileRepository;
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private ProfileRepository profileRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ProfileEntity entity = profileRepository.findByUsername(username);
+        if (entity == null) {
+            throw new ResourceNotFoundException("User not found ");
+        }
+        return new CustomUserDetails(entity);
+    }
+}
